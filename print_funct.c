372b098dd2a054bfdd3187c5a59461da1e2a9d94
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-11-09 14:26:57 +0100

    added _printf with first function and main.h

---
+++ print_funct.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_c - prints a string
+ * @args: character to print
+ * Return: number of characters (1)
+ */
+
+int print_c(va_list args)
+{
+	char e = (char)va_arg(args, int);
+
+	_putchar(e);
+	return (1);
+}
+/**
+ * print_str - prints a string
+ * @args: string to print
+ * Return: number of characters
+ */
+
+int print_str(va_list args)
+{
+	int i = 0;
+	char *str = va_arg(args, char *);
+
+	if (str != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
+	return (i);
+}
